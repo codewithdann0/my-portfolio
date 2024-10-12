b089d6bf0f3b8009b1be8971fd94f1367dfcfe0a
Daniel Tigistu <166249731+codewithdann0@users.noreply.github.com>
2024-10-12 01:30:35 -0700

    the hero section is add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Hero/>
         <Navbar/>
         {children}
       </body>
